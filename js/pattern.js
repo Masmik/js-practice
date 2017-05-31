70e7ded811e01efa0e6a13801d5994d1203cbdd5
Tania Maslivets <t.mykhailiuk1@gmail.com>
2017-05-31 20:10:57 +0300

    WIP

--- js/pattern.js
+++ js/pattern.js
@@ -1,30 +1,21 @@
-'use strict';
-var validate = function () {
-    var forms = document.getElementsByTagName('form');
-
-
-    for (var i = 0; i < forms.length; i++) {
-        forms[i].addEventListener('submit', validation);
-    }
+var modulValidation = (function (form) {
 
     var rule_errors = {
-        required: "Поле не может быть пустым",
-        needName: "Введите имя",
-        phone: "Не верный номер телефона",
-        email: "Странный имейл",
-        equalWith: "Пароль не совпадает",
-        password: "Пароль слижком легкий",
-        city: "Укажите город",
-        house: "Укажите дом",
-        appartment: "Квартира ?",
-        ccName: "Имя на краточке должно совподать с именем введенное вверху",
-        ccNumber: "Идите лесом",
-        ccExp: "Дата истичения срока действия",
-        ccCvc: "Этот код никому нельзя давать, но нам можно!",
-        checked: "Чекните это поле ))"
+        required: 'Поле не может быть пустым',
+        needName: 'Введите имя',
+        phone: 'Не верный номер телефона',
+        email: 'Странный имейл',
+        equalWith: 'Пароль не совпадает',
+        password: 'Пароль слижком легкий',
+        city: 'Укажите город',
+        house: 'Укажите дом',
+        appartment: 'Квартира ?',
+        ccName: 'Имя на краточке должно совподать с именем введенное вверху',
+        ccNumber: 'Укажите номер карты',
+        ccExp: 'Дата истичения срока действия',
+        ccCvc: 'Этот код никому нельзя давать, но нам можно!',
+        checked: 'Чекните это поле ))'
     };
-
-
     var rules = {
         required: function (el) {
             if (el.value !== '') {
@@ -45,8 +36,8 @@ var validate = function () {
             return reg.test(el.value);
         },
         equalWith: function (el) {
-            var dataPassword = el.getAttribute("data-equalId"),
-            pass = document.getElementById(dataPassword).value;
+            var dataPassword = el.getAttribute('data-equalId'),
+                pass = document.getElementById(dataPassword).value;
             if (el.value === pass) {
                 return true;
             }
@@ -83,11 +74,11 @@ var validate = function () {
                 return false;
             } else {
                 var d = new Date(),
-                currentYear = d.getFullYear(),
-                currentMonth = d.getMonth() + 1,
-                parts = el.value.split('/'),
-                year = parseInt(parts[1], 10) + 2000,
-                month = parseInt(parts[0], 10);
+                    currentYear = d.getFullYear(),
+                    currentMonth = d.getMonth() + 1,
+                    parts = el.value.split('/'),
+                    year = parseInt(parts[1], 10) + 2000,
+                    month = parseInt(parts[0], 10);
                 if (year < currentYear || (year === currentYear && month < currentMonth)) {
                     return false;
                 }
@@ -108,39 +99,35 @@ var validate = function () {
     };
 
     function needValidate(el) {
-        if (el.getAttribute("data-rule")) {
+        if (el.getAttribute('data-rule')) {
             return true;
         }
         return false;
     }
 
     function showErrors(arr) {
+        console.log(arr);
+
         for (var i = 0, arrLength = arr.length; i < arrLength; i++) {
 
             arr[i].el.setAttribute('title', rule_errors[arr[i].error_idx]);
 
         }
 
-        $("[title]").tooltip({
+        $('[title]').tooltip({
             position: {
-                my: "left top",
-                at: "right+5 top-5",
-                collision: "none"
+                my: 'left top',
+                at: 'right+5 top-5',
+                collision: 'none'
             }
         });
 
-        $("[title]").tooltip("open");
-
-
+        $('[title]').tooltip('open');
     }
 
-    function validation(e) {
-
-        e.preventDefault();
-        var errors = [],
-
-        inputs = this.elements;
-        for (var i = 0, inputLength = inputs.length ; i < inputLength; i++) {
+    function validation(inputs) {
+        var errors = [];
+        for (var i = 0, inputLength = inputs.length; i < inputLength; i++) {
 
             if (!needValidate(inputs[i])) {
                 continue;
@@ -163,13 +150,26 @@ var validate = function () {
             }
         }
         if (errors.length > 0) {
-            e.preventDefault();
             showErrors(errors);
         }
     }
 
-};
+    return {
+        needValidate: needValidate,
+        showErrors: showErrors,
+        validation: validation
+    }
+}());
 
-$(document).ready(validate);
 
+document.addEventListener('DOMContentLoaded', function () {
+    var forms = document.getElementsByTagName('form');
+    var requireForm = forms[0];
 
+    requireForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        var inputs = this.elements;
+        modulValidation.validation(inputs);
+    });
+
+});
